d94f9de5f26c6669b503d17721c1e66ebbfd717d
LopeKai <kaiqueoakley35@hotmail.com>
2022-09-14 11:14:11 -0300

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/background-galaxy.png')"
+      }
+    },
+  },
+  plugins: [],
+}
